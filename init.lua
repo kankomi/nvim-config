74891e96d9bcc553d2964123dcf213eea9a3c2e7
Patrick Gross <patrick.gross@de.bosch.com>
2024-04-02 07:35:01 +0200

    added svelte and emmet language server

--- init.lua
+++ init.lua
@@ -558,6 +558,8 @@ require('lazy').setup({
         tailwindcss = {},
         htmx = {},
         tsserver = {},
+        svelte = {},
+        emmet_language_server = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
