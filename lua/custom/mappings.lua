70bf77d8a34d917bc402b85baa4c11b7ca16fd4e
Patrick Gross <patrick.gross@de.bosch.com>
2024-04-02 07:58:07 +0200

    more copilot mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,3 +1,4 @@
+-- General
 vim.keymap.set('n', '<leader>rw', 'yiw:%s/<C-r>"//g<Left><Left>', { desc = 'Replace word' })
 vim.keymap.set('v', '<leader>rw', 'y:%s/<C-r>"//g<Left><Left>', { desc = 'Replace highlighted text' })
 vim.keymap.set('n', '<leader>x', ':bd<cr>', { desc = 'Close buffer' })
@@ -6,8 +7,15 @@ vim.keymap.set('n', '<M-l>', '<cmd> bn<cr>', { desc = 'Next buffer' })
 vim.keymap.set('n', '<M-h>', '<cmd> bp<cr>', { desc = 'Previous buffer' })
 vim.keymap.set('i', 'jj', '<esc>', { desc = 'Return to normal mode' })
 vim.keymap.set('n', '<C-s>', '<cmd> w<cr>', { desc = 'Save file' })
+
+-- Conform
 vim.keymap.set('n', '<leader>fm', function()
   local bufnr = vim.api.nvim_get_current_buf()
   require('conform').format { bufnr = bufnr }
 end, { desc = 'Format current buffer' })
-vim.keymap.set('i', '<esc>', '<Plug>(copilot-dismiss)', { desc = 'Dismiss copilt suggestion' })
+
+-- Copilot
+vim.keymap.set('i', '<esc>', '<Plug>(copilot-dismiss)', { desc = 'Dismiss copilot suggestion' })
+vim.keymap.set('i', '<M-ä>', '<Plug>(copilot-next)', { desc = 'Next suggestion' })
+vim.keymap.set('i', '<M-ö>', '<Plug>(copilot-previous)', { desc = 'Previous suggestion' })
+vim.keymap.set('i', '<M-c>', '<Plug>(copilot-suggest)', { desc = 'Suggest completion' })
