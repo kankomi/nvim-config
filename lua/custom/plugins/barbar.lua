bcaec70b3d9bc966ee3b27d8a9da2a7366be5534
Patrick Gross <patrick.gross@de.bosch.com>
2024-04-16 11:41:47 +0200

    added barbar shortcut

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -10,6 +10,7 @@ return {
     vim.keymap.set('n', '<A-,>', ':BufferPrevious<CR>', { silent = true })
     vim.keymap.set('n', '<A-.>', ':BufferNext<CR>', { silent = true })
     vim.keymap.set('n', '<leader>x', '<cmd>BufferClose<cr>', { desc = 'Close buffer' })
+    vim.keymap.set('n', '<leader>bp', '<cmd>BufferPick<cr>', { desc = 'Pick buffer' })
   end,
   opts = {},
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
