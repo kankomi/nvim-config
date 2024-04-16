a8295cbf419a2cc84abad26462981c1a005fe4ae
Patrick Gross <patrick.gross@de.bosch.com>
2024-04-16 15:06:55 +0200

    updated mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,14 +2,12 @@
 vim.keymap.set('n', '<leader>rw', 'yiw:%s/<C-r>"//gc<Left><Left><Left>', { desc = 'Replace word' })
 vim.keymap.set('v', '<leader>rw', 'y:%s/<C-r>"//gc<Left><Left><Left>', { desc = 'Replace highlighted text' })
 vim.keymap.set('n', ';', ':', { desc = 'Open command line' })
--- vim.keymap.set('n', '<M-l>', '<cmd> bn<cr>', { desc = 'Next buffer' })
--- vim.keymap.set('n', '<M-h>', '<cmd> bp<cr>', { desc = 'Previous buffer' })
 vim.keymap.set('i', 'jj', '<esc>', { desc = 'Return to normal mode' })
 vim.keymap.set('n', '<C-s>', '<cmd> w<cr>', { desc = 'Save file' })
 vim.keymap.set('n', '<M-v>', '<C-v>', { desc = 'Visual Block Mode' })
 vim.keymap.set('n', '<leader>y', '<C-">+y', { desc = 'Yank to clipboard', silent = true })
 vim.keymap.set('v', '<leader>y', '<C-">+y', { desc = 'Yank to clipboard', silent = true })
-vim.keymap.set('n', '<leader>p', '"*p', { desc = 'Paste from clipboard', silent = true })
+vim.keymap.set('n', '<leader>p', '<C-">*p', { desc = 'Paste from clipboard', silent = true })
 vim.keymap.set('n', '<M-j>', '<cmd>cnext<cr>', { desc = 'CNext', silent = true })
 vim.keymap.set('n', '<M-k>', '<cmd>cprevious<cr>', { desc = 'CPrev', silent = true })
 
@@ -18,9 +16,3 @@ vim.keymap.set('n', '<leader>fm', function()
   local bufnr = vim.api.nvim_get_current_buf()
   require('conform').format { bufnr = bufnr }
 end, { desc = 'Format current buffer' })
-
--- Copilot
-vim.keymap.set('i', '<esc>', '<Plug>(copilot-dismiss)', { desc = 'Dismiss copilot suggestion' })
-vim.keymap.set('i', '<M-ä>', '<Plug>(copilot-next)', { desc = 'Next suggestion' })
-vim.keymap.set('i', '<M-ö>', '<Plug>(copilot-previous)', { desc = 'Previous suggestion' })
-vim.keymap.set('i', '<M-c>', '<Plug>(copilot-suggest)', { desc = 'Suggest completion' })
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,9 @@
 return {
   'github/copilot.vim',
+  keys = {
+    { mode = 'i', '<esc>', '<Plug>(copilot-dismiss)', desc = 'Dismiss copilot suggestion' },
+    { mode = 'i', '<M-ä>', '<Plug>(copilot-next)', desc = 'Next suggestion' },
+    { mode = 'i', '<M-ö>', '<Plug>(copilot-previous)', desc = 'Previous suggestion' },
+    { mode = 'i', '<M-c>', '<Plug>(copilot-suggest)', desc = 'Suggest completion' },
+  },
 }
