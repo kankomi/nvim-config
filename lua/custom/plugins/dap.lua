424bf4dd9ebc86dc938b583c221417c121e36c36
Patrick <patrick@pgross.de>
2024-05-20 13:43:51 +0200

    updated dap settings

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -1,15 +1,14 @@
 return {
   'mfussenegger/nvim-dap',
+  dependencies = {
+    'theHamsta/nvim-dap-virtual-text',
+    'Weissle/persistent-breakpoints.nvim',
+    'leoluz/nvim-dap-go',
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio'
+  },
   config = function()
-    local dap = require 'dap'
-    -- dap.adapters.delve = {
-    --   type = 'server',
-    --   port = '${port}',
-    --   executable = {
-    --     command = 'dlv',
-    --     args = { 'dap', '-l', '127.0.0.1:${port}' },
-    --   },
-    -- }
+    local dap, ui = require 'dap', require 'dapui'
     dap.adapters.go = {
       type = 'executable',
       command = 'node',
@@ -25,13 +24,92 @@ return {
         dlvToolPath = vim.fn.exepath 'dlv', -- Adjust to where delve is installed
       },
     }
-    -- dap.configurations.go = {
-    --   {
-    --     type = 'delve',
-    --     name = 'My Debug',
-    --     request = 'launch',
-    --     program = './${relativeFileDirName}',
-    --   },
-    -- }
+
+    require('dap-go').setup()
+    require('nvim-dap-virtual-text').setup {}
+    vim.g.dap_virtual_text = true
+
+    require('persistent-breakpoints').setup {
+      load_breakpoints_event = { 'BufReadPost' },
+    }
+    ui.setup()
+    dap.listeners.after.event_initialized['dapui_config'] = function()
+      ui.open()
+    end
+    dap.listeners.before.event_terminated['dapui_config'] = function()
+      ui.close()
+    end
+    dap.listeners.before.event_exited['dapui_config'] = function()
+      ui.close()
+    end
+
+    vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
   end,
+  keys = {
+    {
+      '<leader>db',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'Toggle Breakpoint',
+    },
+    {
+      '<leader>dt',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = 'Toggle Debug UI',
+    },
+    {
+      '<leader>dr',
+      function()
+        require('dap').run_to_cursor()
+      end,
+      desc = 'Run to cursor'
+    },
+    {
+      '<F1>',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Debug Continue',
+    },
+    {
+      '<F2>',
+      function()
+        require('dap').step_into()
+      end,
+      desc = 'Debut Step Into',
+    },
+    {
+      '<F3>',
+      function()
+        require('dap').step_over()
+      end,
+      desc = 'Debug Step Over',
+    },
+    {
+      '<F4>',
+      function()
+        require('dap').step_out()
+      end,
+      desc = 'Debug Step Out',
+    },
+    {
+      '<F9>',
+      function()
+        require('dap').restart()
+      end,
+      desc = 'Debug Restart',
+    },
+    {
+      '<F10>',
+      function()
+        require('dap').close()
+        require('dapui').close()
+      end,
+      desc = 'Debug Stop',
+    },
+  },
 }
