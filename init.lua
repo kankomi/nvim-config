d10f8feaaaa00beb37750f2d1afd1a400243679a
Patrick Gross <patrick.gross@de.bosch.com>
2024-05-22 12:12:39 +0200

    formatting

--- init.lua
+++ init.lua
@@ -160,7 +160,7 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'numToStr/Comment.nvim',    opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -180,9 +180,11 @@ require('lazy').setup({
     },
     config = function()
       require('gitsigns').setup()
-      vim.keymap.set('n', '<leader>gh', '<cmd>lua require"gitsigns".toggle_signs()<CR>', { desc = 'Toggle [G]it [H]ighlights' })
+      vim.keymap.set('n', '<leader>gh', '<cmd>lua require"gitsigns".toggle_signs()<CR>',
+        { desc = 'Toggle [G]it [H]ighlights' })
       vim.keymap.set('n', '<leader>gj', '<cmd>lua require"gitsigns".next_hunk()<CR>', { desc = 'Jump to [N]ext [H]unk' })
-      vim.keymap.set('n', '<leader>gk', '<cmd>lua require"gitsigns".prev_hunk()<CR>', { desc = 'Jump to [P]revious [H]unk' })
+      vim.keymap.set('n', '<leader>gk', '<cmd>lua require"gitsigns".prev_hunk()<CR>',
+        { desc = 'Jump to [P]revious [H]unk' })
     end,
   },
 
@@ -201,7 +203,7 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  { -- Useful plugin to show you pending keybinds.
+  {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
@@ -247,7 +249,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
